0a8c166f32997bf64b42050183e6d06bfd1aa658
kZilla <kzilla@vn-z.zilla>
2024-05-25 23:08:18 +0300

    API Call to view stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,11 +6,19 @@ Routes for the blueprint
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-
-classes = {"users": "User", "places": "Place", "states": "State",
-           "cities": "City", "amenities": "Amenity",
-           "reviews": "Review"}
+classes = {"users": User,
+           "places": Place,
+           "states": State,
+           "cities": City,
+           "amenities": Amenity,
+           "reviews": Review}
 
 
 # define route /status
@@ -21,10 +29,11 @@ def status():
 
 
 # define route /stats
-@app_views.route('/stats', method=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def stats():
     ''' retrieves the number of each objects by type '''
     stats_dict = {}
     for cls in classes:
-        stats_dict[cls] = storage.count(classess[cls])
+        stats_dict[cls] = storage.count(classes[cls])
+    # stats_dict = {"states": storage.count(State)} test dummy
     return jsonify(stats_dict)
