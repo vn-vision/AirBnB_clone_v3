3f2107e0cd0488153c30351706b33f38951a60f5
kZilla <kzilla@vn-z.zilla>
2024-05-25 22:00:46 +0300

    API: app

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Routes for the blueprint
+'''
+
+from api.v1.views import app_views
+from flask import jsonify
+
+# define route /status
+@app_views.route('/status', methods=['GET'])
+def status():
+    ''' returns status '''
+    return jsonify({"status":"OK"})
