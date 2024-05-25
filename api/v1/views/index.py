1d0cffafba05b9eeb80398f87ac3c64f946e6d33
kZilla <kzilla@vn-z.zilla>
2024-05-25 22:19:14 +0300

    What are the stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@ Routes for the blueprint
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
 # define route /status
@@ -12,3 +18,13 @@ from flask import jsonify
 def status():
     ''' returns status '''
     return jsonify({"status": "OK"})
+
+
+# define route /stats
+@app_views.route('/stats', method=['GET'])
+def stats():
+    ''' retrieves the number of each objects by type '''
+    stats_dict = {}
+    for cls in classes:
+        stats_dict[cls] = storage.count(classess[cls])
+    return jsonify(stats_dict)
