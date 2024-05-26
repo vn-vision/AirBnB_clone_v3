b3db0cb464610c5eebe09daa3474c33364569349
Karushi536 <karushiabigael@gmail.com>
2024-05-26 22:10:03 +0300

    tried question 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-''' this is a package '''
 from flask import Blueprint
-
-# blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-if app_views is not None:
-    from api.v1.views.index import *
+from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
+from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
+from api.v1.views.users import *  # noqa
+from api.v1.views.places import *  # noqa
+from api.v1.views.places_reviews import *  # noqa
+from api.v1.views.places_amenities import *  # noqa
