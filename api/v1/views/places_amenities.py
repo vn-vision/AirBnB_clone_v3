6c2a0d3599ba46550a18e1efd60e790bdb66caef
Karushi536 <karushiabigael@gmail.com>
2024-05-27 19:16:44 +0300

    fixing pycode style errors

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -9,8 +9,9 @@ from models.place import Place
 from models.amenity import Amenity
 from os import getenv
 
-# Retrieves the list of all Amenity objects of a Place
-@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/places/<place_id>/amenities',
+                 methods=['GET'], strict_slashes=False)
 def get_place_amenities(place_id):
     place = storage.get(Place, place_id)
     if not place:
@@ -18,11 +19,13 @@ def get_place_amenities(place_id):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         amenities = place.amenities
     else:
-        amenities = [storage.get(Amenity, amenity_id) for amenity_id in place.amenity_ids]
+        amenities = [storage.get(Amenity, amenity_id)
+                     for amenity_id in place.amenity_ids]
     return jsonify([amenity.to_dict() for amenity in amenities])
 
-# Deletes a Amenity object to a Place
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
     place = storage.get(Place, place_id)
     if not place:
@@ -30,7 +33,7 @@ def delete_place_amenity(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         if amenity not in place.amenities:
             abort(404)
@@ -39,12 +42,13 @@ def delete_place_amenity(place_id, amenity_id):
         if amenity_id not in place.amenity_ids:
             abort(404)
         place.amenity_ids.remove(amenity_id)
-    
+
     storage.save()
     return jsonify({}), 200
 
-# Links a Amenity object to a Place
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
 def link_place_amenity(place_id, amenity_id):
     place = storage.get(Place, place_id)
     if not place:
@@ -61,6 +65,6 @@ def link_place_amenity(place_id, amenity_id):
         if amenity_id in place.amenity_ids:
             return jsonify(amenity.to_dict()), 200
         place.amenity_ids.append(amenity_id)
-    
+
     storage.save()
     return jsonify(amenity.to_dict()), 201
